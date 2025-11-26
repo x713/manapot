f68643d95a7268ddbb45f3bce02ff13116048c7a
x713 <unx0rz@gmail.com>
2025-11-26 21:19:37 +0000

    Flask app with database and deploy scripts

---
+++ templates/sample_vars.sh
@@ -0,0 +1,12 @@
+export PROJECT_ID=$(gcloud config get-value project)
+
+export REGION="set-region-name"
+export ZONE="set-zone_name"
+
+export REPO_NAME="change-my-reponame"
+export CLUSTER_NAME="change-my-clustername"
+export SQL_INSTANCE="change-my-sql-instancename"
+export DB_NAME="change_app_db_name"
+export DB_USER="change_app_username"
+
+export DB_PASS="change_password"
